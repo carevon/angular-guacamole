1f824a1125fe3c5ffb891e37d687e24c67da661e
Logon Aluno <logonaluno@local.com>
2019-08-06 20:57:36 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'primeiroProjeto';
+  title = 'Cachorreira';
+  imageURL:string = "https://4.bp.blogspot.com/-Ne-KHti1gGM/V-Y1IGKqpTI/AAAAAAAAHS8/DwVvUXm9D0cRUXAMR3PMmVGG8y5d4scZwCLcB/s1600/pica%2Bpau.jpg"
 }
